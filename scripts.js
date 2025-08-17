6672d2f807d83720d06a8b7016a399b7e62cac5f
EduardoCabrejas <educabre8991@gmail.com>
2025-08-17 19:55:26 -0300

    add[header, home-section]: done structure, styles & scrippts]

---
+++ scripts.js
@@ -0,0 +1,62 @@
+const burgerMenu = document.querySelector("#burger-menu");
+const links = document.querySelector("#nav-links");
+
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("open");
+
+  const expanded = burgerMenu.getAttribute("aria-expanded") === "true";
+  burgerMenu.setAttribute("aria-expanded", !expanded);
+
+  links.classList.toggle("show");
+});
+
+const endDate = new Date("2025-09-30T23:59:59").getTime();
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const distance = endDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days-box").textContent = days
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("hours").textContent = hours
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("minutes").textContent = minutes
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("seconds").textContent = seconds
+    .toString()
+    .padStart(2, "0");
+
+  if (distance < 0) {
+    clearInterval(timer);
+    document.querySelector(".time-container").innerHTML =
+      "<p class='dark-text'>Offer expired</p>";
+  }
+}
+
+updateCountdown();
+const timer = setInterval(updateCountdown, 1000);
+
+function moveContent() {
+  const timeContainer = document.querySelector(".time-container");
+  const toggleP = document.querySelector("#toggle-p");
+  const hgroup = document.querySelector(".content-column hgroup");
+
+  if (window.innerWidth <= 1024) {
+    hgroup.insertBefore(timeContainer, toggleP.nextSibling);
+  } else {
+    document.querySelector(".home-grid").appendChild(timeContainer);
+  }
+}
+
+window.addEventListener("resize", moveContent);
+window.addEventListener("DOMContentLoaded", moveContent);
